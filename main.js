77f8ea630c103b87f31598f40a769334c787f64b
Yiwen Zhang <yiwenmary@gmail.com>
2024-03-08 22:21:14 -0500

    create states.js and colors.js, organize code into 3 module files, and import across files

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { states } from './states.js';
+import { colors } from './colors.js';
+
 const np_container = document.getElementById('np-container');
 
 // Get parkCode for 60+ national parks from NPS-Unit-List.json in the same folder
@@ -27,59 +30,6 @@ async function getAllParkCode () {
 // getAllParkCode();
 // console.log(np_codes)
 
-// Create an array of state codes and names
-const states = [
-    { code: 'AL', name: 'Alabama' },
-    { code: 'AK', name: 'Alaska' },
-    { code: 'AZ', name: 'Arizona' },
-    { code: 'AR', name: 'Arkansas' },
-    { code: 'CA', name: 'California' },
-    { code: 'CO', name: 'Colorado' },
-    { code: 'CT', name: 'Connecticut' },
-    { code: 'DE', name: 'Delaware' },
-    { code: 'FL', name: 'Florida' },
-    { code: 'GA', name: 'Georgia' },
-    { code: 'HI', name: 'Hawaii' },
-    { code: 'ID', name: 'Idaho' },
-    { code: 'IL', name: 'Illinois' },
-    { code: 'IN', name: 'Indiana' },
-    { code: 'IA', name: 'Iowa' },
-    { code: 'KS', name: 'Kansas' },
-    { code: 'KY', name: 'Kentucky' },
-    { code: 'LA', name: 'Louisiana' },
-    { code: 'ME', name: 'Maine' },
-    { code: 'MD', name: 'Maryland' },
-    { code: 'MA', name: 'Massachusetts' },
-    { code: 'MI', name: 'Michigan' },
-    { code: 'MN', name: 'Minnesota' },
-    { code: 'MS', name: 'Mississippi' },
-    { code: 'MO', name: 'Missouri' },
-    { code: 'MT', name: 'Montana' },
-    { code: 'NE', name: 'Nebraska' },
-    { code: 'NV', name: 'Nevada' },
-    { code: 'NH', name: 'New Hampshire' },
-    { code: 'NJ', name: 'New Jersey' },
-    { code: 'NM', name: 'New Mexico' },
-    { code: 'NY', name: 'New York' },
-    { code: 'NC', name: 'North Carolina' },
-    { code: 'ND', name: 'North Dakota' },
-    { code: 'OH', name: 'Ohio' },
-    { code: 'OK', name: 'Oklahoma' },
-    { code: 'OR', name: 'Oregon' },
-    { code: 'PA', name: 'Pennsylvania' },
-    { code: 'RI', name: 'Rhode Island' },
-    { code: 'SC', name: 'South Carolina' },
-    { code: 'SD', name: 'South Dakota' },
-    { code: 'TN', name: 'Tennessee' },
-    { code: 'TX', name: 'Texas' },
-    { code: 'UT', name: 'Utah' },
-    { code: 'VT', name: 'Vermont' },
-    { code: 'VA', name: 'Virginia' },
-    { code: 'WA', name: 'Washington' },
-    { code: 'WV', name: 'West Virginia' },
-    { code: 'WI', name: 'Wisconsin' },
-    { code: 'WY', name: 'Wyoming' }
-];
 
 // Populate the state filter dropdown
 const stateFilter = document.getElementById('state-filter');
@@ -90,62 +40,6 @@ states.forEach((state) => {
     option.textContent = state.name;
     stateFilter.appendChild(option);
 })
-
-// color for each state
-const colors = {
-    "AL": "#E0B3FF",
-    "AK": "#F5F5F5",
-    "AZ": "#FDDFDF",
-    "AR": "#E6E6E6",
-    "CA": "#FFCC66",  // "#FFB3A3",
-    "CO": "#FFFFE0",
-    "CT": "#FFFDB3",
-    "DE": "#FFCCB3",
-    "FL": "#B3FFF5",
-    "GA": "#D9FFDB",
-    "HI": "#85C1E9", // "#B3FFF5",
-    "ID": "#FFFAE6",
-    "IL": "#FFCCFF",
-    "IN": "#FFCCCC",
-    "IA": "#FFFDB3",
-    "KS": "#B3FFD9",
-    "KY": "#FFF5B3",
-    "LA": "#E5E5E5",
-    "ME": "#E0CCFF",
-    "MD": "#D9D9D9",
-    "MA": "#B3E5FF",
-    "MI": "#E6FFB3",
-    "MN": "#B3FFF5",
-    "MS": "#B3FFCC",
-    "MO": "#FFD9DC",
-    "MT": "#B3F5FF",
-    "NE": "#CCB3FF",
-    "NV": "#FFB3D9",
-    "NH": "#B3FFF0",
-    "NJ": "#FFD9DC",
-    "NM": "white",
-    "NY": "#CCB3FF",
-    "NC": "#DEFDE0",
-    "ND": "#E6E0D4",
-    "OH": "#FFEEDD",  // "#FFE5B3",
-    "OK": "#D9FFB3",
-    "OR": "#B3A9FF",  // "#B3ECFF",
-    "PA": "#FFD9B3",
-    "RI": "#F5F5F5",
-    "SC": "#EAEDA1",
-    "SD": "#F8D5A3",
-    "TN": "#98D7A5",
-    "TX": "#FFFFE0",
-    "UT": "#FFB3B3",
-    "VT": "#FCEAFF",
-    "VA": "#F4E7DA",
-    "VI": "#66CCFF", 
-    "WA": "#86E3FF",  // "#B3FFFA",
-    "WV": "#DEF3FD",
-    "WI": "#FCF7DE",
-    "WY": "#B3D9FF"
-};
-
 
 const main_type = Object.keys(colors);
 
